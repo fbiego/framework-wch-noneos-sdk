cca9dca34848730c1871ab0725f7ddcf4a6d6391
Felix Biego <45796740+fbiego@users.noreply.github.com>
2025-04-20 16:17:00 +0300

    update ch585

---
+++ Core/ch585/core_riscv.c
@@ -0,0 +1,13 @@
+/********************************** (C) COPYRIGHT  *******************************
+ * File Name          : core_riscv.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2024/07/25
+ * Description        : CH585 Series RISC-V Core Peripheral Access Layer Source File
+ *********************************************************************************
+ * Copyright (c) 2024 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+
